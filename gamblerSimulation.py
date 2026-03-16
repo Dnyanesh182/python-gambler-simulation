4d364b33cd755855745813aa86b2226297989aa0
Dnyaneshwar Potdar <dnyaneshwarpotdar2732001@gmail.com>
2026-03-16 13:58:53 +0530

    feat(gambler-simulation): update stake based on win or loss outcome

--- gamblerSimulation.py
+++ gamblerSimulation.py
@@ -1,4 +1,4 @@
-# UC2 – Simulate a Single Bet (Win or Loss)
+# UC3 – Update Stake Based on Win or Loss
 
 import random
 
@@ -9,6 +9,10 @@ bet_amount = 1
 bet_result = random.randint(0, 1)
 
 if bet_result == 1:
+    stake += bet_amount
     print("Gambler wins the bet")
 else:
+    stake -= bet_amount
     print("Gambler loses the bet")
+
+print("Updated Stake:", stake)
